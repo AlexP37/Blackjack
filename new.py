406626f75e120e4acc719cb652959f2facb536bc
Alexander Photios <alexander.photios@student.tsc.nsw.edu.au>
2020-10-22 17:17:26 +1100

    Hit, Stand, Textboxs, Display, Dealer and Bust Functionality

--- new.py
+++ new.py
@@ -17,7 +17,7 @@ gameDisplay.fill((255,255,255))
 
 suits = ["h","d","c","s"]
 cardTypes = ["a","2","3","4","5","6","7","8","9","10","j","q","k"]
-valuesForCards = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+valuesForCards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
 deck = []
 hand = []
@@ -31,20 +31,57 @@ class Card:
         self.cardType = cardType
 
 class Hand:
-    def __init__(self):
+    def __init__(self, player):
+        self.player = player
         self.value = 0
         self.cards = [deck[0], deck[1]]
         deck.remove(deck[0])
         deck.remove(deck[0])
-        for i in self.cards:
-            self.value += int(self.cards[self.cards.index(i)].value)
+        self.value += self.cards[0].value
+        if self.cards[-1].name in ["ha","da","ca","sa"] and (self.value + 11) > 21:
+            self.value += 1
+        else:
+            self.value += self.cards[-1].value
 
     def hit(self):
         self.cards.append(deck[0])
         deck.remove(deck[0])
-        self.value = 0
+        if self.cards[-1].name in ["ha","da","ca","sa"] and (self.value + 11) > 21:
+            self.value += 1
+        else:
+            self.value += self.cards[-1].value
+ 
+    def display(self):
+        cardsInDeckNames = ""
+        coords = (20,200)
         for i in self.cards:
-            self.value += int(self.cards[self.cards.index(i)].value)
+            cardsInDeckNames += i.name + " "
+        if self.player == "dealer":
+            coords = (20,100)
+        textBoxClear(str(self.value) + ": " + cardsInDeckNames, 50, coords, (0,0,0))
+
+class TextBox:
+    def __init__(self, text, size, position, color):
+        self.text = text
+        self.size = size
+        self.position = position
+        self.color = color
+        self.display()
+
+    def display(self):
+        font = pygame.font.Font("LemonMilk.otf", self.size)
+        text = font.render(self.text, True, self.color)
+        textRect = text.get_rect()
+        textRect.midleft = self.position
+        gameDisplay.blit(text, textRect)
+        self.rect = textRect
+
+def textBoxClear(text, size, position, color):
+    font = pygame.font.Font('LemonMilk.otf', size)
+    text = font.render(text, True, color)
+    textRect = text.get_rect()
+    textRect.midleft = position
+    gameDisplay.blit(text, textRect)
 
 def createDeck():
     for s in suits:
@@ -56,16 +93,39 @@ def createDeck():
 def hitCard(who):
     who.hit()
 
+def displayHand(who):
+    who.display()
+
+def updateScreen():
+    gameDisplay.fill((255,255,255))
+    if stand == True:
+        standBox.color = (0,165,0)
+    displayHand(userHand)
+    displayHand(dealerHand)
+    # betBox.display()
+    hitBox.display()
+    standBox.display()
+    splitBox.display()
+
 createDeck()
 random.shuffle(deck)
-userHand = Hand()
-dealerhand = Hand()
+
+userHand = Hand("user")
+dealerHand = Hand("dealer")
+
+# betBox = TextBox("Bet", 100, (20,400), (0,0,0))
+hitBox = TextBox("Hit", 100, (20,500), (0,0,0))
+standBox = TextBox("Stand", 100, (20,600), (0,0,0))
+splitBox = TextBox("Split", 100, (20,700), (0,0,0))
 
 end = False
+bust = False
+stand = False
+
+updateScreen()
+
 while end == False:
     clock.tick(60)
-    img = pygame.image.load('background.jpg')
-    gameDisplay.blit(img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
@@ -80,12 +140,30 @@ while end == False:
                 quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
-            hitCard(userHand)
-
+            if hitBox.rect.collidepoint(mx, my):
+                hitBox.color = (165,0,0)
+                if userHand.value < 21:
+                    hitCard(userHand)
+                if userHand.value > 21:
+                    bust = True
+            elif standBox.rect.collidepoint(mx, my):
+                stand = True
+            elif splitBox.rect.collidepoint(mx, my):
+                print("lmao")
+            updateScreen()
+        if event.type == pygame.MOUSEBUTTONUP:
+            mx, my = pygame.mouse.get_pos()
+            if hitBox.rect.collidepoint(mx, my):
+                hitBox.color = (0,0,0)
+            updateScreen()
+    if bust == True or stand == True:
+        if dealerHand.value < 17:
+            hitCard(dealerHand)
+            updateScreen()
     pygame.display.update()
 
 for i in userHand.cards:
-    print(userHand.cards[userHand.cards.index(i)].name)
+    print(i.name)
 # Game Over
 print("Awaiting End")
 pygame.quit()
